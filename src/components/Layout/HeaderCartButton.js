4ec187aa6b47a92da1b1480ed5c33d355292b80b
Xuan Dien <xdienw@gmail.com>
2022-08-04 09:45:29 +0700

    hoan thanh khoa hocj

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon"
 import CartContext from "../store/cart-context";
 import classes from "./HeaderCartButton.module.css"
@@ -8,7 +8,21 @@ function HeaderCartButton(props) {
         return currentNumber + item.amount;
     },0);
 
-    return <button className={classes.button} onClick={props.onClick}>
+    
+    const [btnIsHighlight, setBtnIsHightLight] = useState(false);
+    const {items} = cartCtx;
+    const btnClasses = `${classes.button} ${btnIsHighlight?classes.bump:''}`
+    useEffect(()=>{
+        setBtnIsHightLight(true);
+        let timer = setTimeout(() => {
+            setBtnIsHightLight(false);
+        }, 300);
+        return () =>{
+            clearTimeout(timer);
+        };
+    },[items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
